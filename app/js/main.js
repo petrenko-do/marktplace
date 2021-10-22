a6a6947d49052195d16a7e7af37cee9a86851338
petrenko-do <densamigos@ukr.net>
2021-10-22 20:29:53 +0200

    end home page

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,31 @@
 $(function () {
   
+  $(".rate-star").rateYo({
+    rating: 4.5,
+    starWidth: "14px",
+    readOnly: true
+  });
+
+
+
+  $('.product-slider__inner').slick({
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="../app/images/icons/arrow-left.png" alt=""></button>' ,
+    nextArrow: '<button class="slick-arrow slick-next"><img src="../app/images/icons/arrow-right.png" alt=""></button>'
+  });
+
+  $('.followers__items').slick({
+    // infinite: true,
+    // adaptiveHeight: true,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  });
+
+
 })
+
+
+
+
+
+var mixer = mixitup('.release__items');
+
