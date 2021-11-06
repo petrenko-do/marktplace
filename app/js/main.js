8b885cbeec044c809fcd0e7f1159fed307b4d121
petrenko-do <densamigos@ukr.net>
2021-11-06 22:39:56 +0100

    added category-page

--- app/js/main.js
+++ app/js/main.js
@@ -12,6 +12,10 @@ $(function () {
     prevArrow: '<button class="slick-arrow slick-prev"><img src="../app/images/icons/arrow-left.png" alt=""></button>' ,
     nextArrow: '<button class="slick-arrow slick-next"><img src="../app/images/icons/arrow-right.png" alt=""></button>'
   });
+  $('.followers__items').slick({
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="../app/images/icons/arrow-left.png" alt=""></button>' ,
+    nextArrow: '<button class="slick-arrow slick-next"><img src="../app/images/icons/arrow-right.png" alt=""></button>'
+  });
 
   $('.followers__items').slick({
     // infinite: true,
@@ -19,6 +23,20 @@ $(function () {
     slidesToShow: 3,
     slidesToScroll: 1
   });
+
+
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 350,
+    from: 30,
+    to: 300,
+    prefix: "$"
+});
+
+
+
+
 
 
 })
